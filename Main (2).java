91a5185d84fc013a3acdd67af8370a077175b120
preethi4444 <59753226+preethi4444@users.noreply.github.com>
2020-01-14 17:35:54 +0530

    Inheritance,composition,constructors

---
+++ Main (2).java
@@ -0,0 +1,11 @@
+public class Main
+{
+    private Main(){
+        
+        System.out.println("Hello World");
+    }
+	public static void main(String[] args) {
+	    Main obj= new Main();
+		
+	}
+}
